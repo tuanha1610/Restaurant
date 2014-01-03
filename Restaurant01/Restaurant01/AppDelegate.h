43b001a9e337d9baa82fc132a371ef6d438cf596
hoang anh tuan <tuanha1610@gmail.com>
2014-01-04 00:42:05 +0800

    ok

---
+++ Restaurant01/Restaurant01/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Restaurant01
+//
+//  Created by MAC on 1/3/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Restaurant01/Restaurant01/Customer.m
@@ -0,0 +1,13 @@
+//
+//  Customer.m
+//  Restaurant01
+//
+//  Created by MAC on 1/4/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import "Customer.h"
+
+@implementation Customer
+
+@end
