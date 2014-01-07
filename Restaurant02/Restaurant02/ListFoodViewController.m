9463e1e35d80e44bbdbbb6e24703f880fd3f9a98
hoang anh tuan <tuanha1610@gmail.com>
2014-01-08 01:08:01 +0700

    aaa

--- Restaurant02/Restaurant02/ListFoodViewController.m
+++ Restaurant02/Restaurant02/ListFoodViewController.m
@@ -7,13 +7,38 @@
 //
 
 #import "ListFoodViewController.h"
-
+#import <Parse/Parse.h>
+#import "Food.h"
 @interface ListFoodViewController ()
 
 @end
 
-@implementation ListFoodViewController
+@implementation ListFoodViewController{
 
+}
+- (id)initWithCoder:(NSCoder *)aCoder
+{
+    self = [super initWithCoder:aCoder];
+    if (self) {
+        // Custom the table
+        
+        // The className to query on
+        self.parseClassName = @"Food";
+        
+        // The key of the PFObject to display in the label of the default cell style
+        self.textKey = @"name";
+        
+        // Whether the built-in pull-to-refresh is enabled
+        self.pullToRefreshEnabled = YES;
+        
+        // Whether the built-in pagination is enabled
+        self.paginationEnabled = NO;
+        
+        // The number of objects to show per page
+        //self.objectsPerPage = 10;
+    }
+    return self;
+}
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,9 +52,65 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(refreshTable:)
+                                                 name:@"refreshTable"
+                                               object:nil];
+}
+- (void)refreshTable:(NSNotification *) notification
+{
+    // Reload the recipes
+    [self loadObjects];
 }
 
-- (void)didReceiveMemoryWarning
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (PFQuery *)queryForTable
+{
+    PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+    
+    // If no objects are loaded in memory, we look to the cache first to fill the table
+    // and then subsequently do a query against the network.
+    /*    if ([self.objects count] == 0) {
+     query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+     }*/
+    
+    //    [query orderByAscending:@"name"];
+    
+    return query;
+}
+
+
+
+// Override to customize the look of a cell representing an object. The default is to display
+// a UITableViewCellStyleDefault style cell with the label being the first key in the object.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object
+{
+    static NSString *simpleTableIdentifier = @"FoodCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    // Configure the cell
+    PFFile *thumbnail = [object objectForKey:@"image"];
+    PFImageView *thumbnailImageView = (PFImageView*)[cell viewWithTag:100];
+    thumbnailImageView.image = [UIImage imageNamed:@"placeholder.jpg"];
+    thumbnailImageView.file = thumbnail;
+    [thumbnailImageView loadInBackground];
+    
+    UILabel *nameLabel = (UILabel*) [cell viewWithTag:101];
+    nameLabel.text = [object objectForKey:@"name"];
+    
+    UILabel *prepTimeLabel = (UILabel*) [cell viewWithTag:102];
+    prepTimeLabel.text = [object objectForKey:@"price"];
+    
+    return cell;
+}- (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
